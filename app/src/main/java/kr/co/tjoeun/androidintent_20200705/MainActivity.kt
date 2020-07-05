2fede3c3beb9b6ef012d1cbe2a556ccbd61cc220
yd27 <dhdbseh5oe@naver.com>
2020-07-05 14:54:54 +0900

    DIAL 기능 완성!

--- app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.androidintent_20200705
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dialBtn.setOnClickListener { {
+        //다이얼버튼 이벤트
+        dialBtn.setOnClickListener {
 
-        } }
+            //입력된 전화번호를 받아오자.
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            //안드로이드에게, 어디에 전화걸지 정보 전달 -> Uri
+            //전화 uri 양식 - tel:010-8241-6502
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            //전화 화면으로 이동하는 Intent
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+
+            //Log.d("String가공확인", "tel:${inputPhoneNum}")
+
+         }
     }
 }
